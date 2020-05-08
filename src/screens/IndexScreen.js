90cc0847cbe7150212c8d043df0428e1763c3057
Timothy Bruns <timothybruns@gmail.com>
2020-05-07 20:36:46 -0700

    adding blog posts

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,14 +1,21 @@
 import React, { useContext } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
-    const value = useContext(BlogContext);
+    const {data, addBlogPost} = useContext(BlogContext);
   
     return (
     <View>
       <Text>Index Screen</Text>
-      <Text>{value}</Text>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList 
+        data={data}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({item}) => {
+            return <Text>{item.title}</Text>
+        }}
+      />
     </View>
   );
 };
